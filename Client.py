cc5d372a0bf6779cdbe4752502cba3455ada5319
b0hu <96330029+b0hu@users.noreply.github.com>
2022-05-23 14:05:07 +0800

    SDN initialization

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 # Send UDP broadcast packets
 MYPORT = 50000
-import sys, time from socket import *
+import sys, time
+from socket import *
 s = socket(AF_INET, SOCK_DGRAM)
 s.bind(('', 0))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
